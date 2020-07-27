24268d5ca0a900b79fe0bf717fab640c0b2c6034
Jameel Amir <jameelozbiz@gmail.com>
2020-07-27 05:55:39 +0300

    fixed issue by converting PNG to RGB only, replaced PILKit with PIL

--- showcoords.py
+++ showcoords.py
@@ -2,8 +2,7 @@ import pylab as plt
 import pyautogui
 import numpy as np
 from matplotlib.text import OffsetFrom
-from pilkit.lib import Image
-from pilkit.processors import TrimBorderColor
+from PIL import Image, ImageChops
 from secret_tokens import screenshotpath
 from back_or_forward_slash import back_or_forward_slash
 def showcoords(click_x=None, click_y=None):
@@ -17,13 +16,14 @@ def showcoords(click_x=None, click_y=None):
     ax = plt.subplots()[1] #should've been fig, ax = plt.subplots() but trying this
     if click_x != None and click_y != None:
         plt.scatter(x=click_x, y=click_y, c='r', s=30)
-    for i in range(0, round(screenshotwidth/100)+1):
-        plt.text(i*100, 0, i, color="yellow", fontsize=5, horizontalalignment='center',bbox=dict(fill=True, edgecolor='red', linewidth=1))
+    separation = 100
+    for i in range(0, screenshotwidth, separation):
+        plt.text(i, 0, i, color="yellow", fontsize=5, horizontalalignment='center',bbox=dict(fill=True, edgecolor='red', linewidth=1))
 
-    for i in range(0, round(screenshotheight/100)):
-        plt.text(0, i*100, i, color="yellow", fontsize=5, verticalalignment='center', bbox=dict(fill=True, edgecolor='red', linewidth=1))
+    for i in range(0, screenshotwidth, separation):
+        plt.text(0, i, i, color="yellow", fontsize=5, verticalalignment='center', bbox=dict(fill=True, edgecolor='red', linewidth=1))
     image = plt.imread(mouseclickfullpath) #matplotlib reading screenshot
-    major_ticks = np.arange(0, screenshotwidth, 100) #arranging (idk, length of width, interval)
+    major_ticks = np.arange(0, screenshotwidth, separation) #arranging (idk, length of width, interval)
     plt.tick_params(labelsize=3.5) #font size
     ax.set_xticks(major_ticks) #setting major ticks
     ax.set_yticks(major_ticks)
@@ -32,7 +32,19 @@ def showcoords(click_x=None, click_y=None):
     plt.imshow(image)
     plt.savefig(mousechartfullpath, dpi=300) #saving figure
     image = Image.open(mousechartfullpath)
-    processor = TrimBorderColor() #automatically trimming until there is content
-    new_img = processor.process(image)
-    new_img.save(mousechartfullpath)
+
+    def trim(im):
+        bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
+        diff = ImageChops.difference(im, bg)
+        diff = ImageChops.add(diff, diff, 2.0, -100)
+        bbox = diff.convert('RGB').getbbox()
+        if bbox:
+            return im.crop(bbox)
+        elif bbox == None: 
+            raise AttributeError("bbox value == None, try converting image into RGB if you haven't already done so.")
+
+    im = Image.open(mousechartfullpath)
+    im = trim(im)
+    im.save(mousechartfullpath)
+
 
