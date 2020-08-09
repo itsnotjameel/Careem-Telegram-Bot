cd9213212ccb2bd1f7102300ac43d373cc318a3a
Jameel Amir <jameelozbiz@gmail.com>
2020-08-09 15:45:05 +0300

    reformatted

--- back_or_forward_slash.py
+++ back_or_forward_slash.py
@@ -1,6 +1,10 @@
 import platform
+
+
 def back_or_forward_slash(directory, filename):
     if platform.system() == "Windows":
-        return r"{directory}\{filename}".format(directory=directory, filename=filename)
+        return r"{directory}\{filename}".format(
+            directory=directory, filename=filename)
     elif platform.system() != "Windows":
-        return r"{directory}/{filename}".format(directory=directory, filename=filename)
+        return r"{directory}/{filename}".format(
+            directory=directory, filename=filename)
